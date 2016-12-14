9415ae5cb110a8ec76a1a208f51a4e6a4b25a51a
Andy <Andy@Snoopy>
2016-12-14 13:24:24 +0800

    ttt

---
+++ src/test/java/csd/starter/Location.java
@@ -0,0 +1,24 @@
+package csd.starter;
+
+public class Location {
+	private int x;
+	private int y;  
+	public Location(int x2, int y2) {
+		// TODO Auto-generated constructor stub
+		this.x = x2;
+		this.y = y2;
+	}
+	public int getX() {
+		return x;
+	}
+	public void setX(int x) {
+		this.x = x;
+	}
+	public int getY() {
+		return y;
+	}
+	public void setY(int y) {
+		this.y = y;
+	}
+	
+}
