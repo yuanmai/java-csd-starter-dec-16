9ec97f38828968d8ed0478c2dde552f33fd8704e
bapl <bapl1016@gmail.com>
2016-12-14 16:18:21 +0800

    add end to end test case

--- src/main/java/csd/starter/Main.java
+++ src/main/java/csd/starter/Main.java
@@ -1,23 +1,34 @@
 package csd.starter;
 
-import java.io.ByteArrayInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.ArrayList;
-import java.util.Scanner;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-
+import java.util.Scanner;
+import static org.junit.Assert.assertEquals;
+import csd.starter.vo.Count;
+import csd.starter.vo.Player;
 public class Main {
+    private static SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy/mm/dd hh");
 
-    public static void main(String[] args) {
-//      String input = "10,10";
-    	System.out.println("Please input location x,y,  ");
-    	Scanner s = new Scanner(System.in).useDelimiter("\\s*,\\s*");
-    	scanner(s); 
-        
+    public static void main(String[] args) throws ParseException {
+    	bookSuccess();
+        //printResult(result);
     }
+
+    public static void bookSuccess() throws ParseException{
+    	String[] inputs=getInput();
+    	Player p1 = new Player(inputs[0]);
+    	Date bookDate = dateFormat.parse(inputs[1]);
+		Count c1 = new Count(Integer.valueOf(inputs[3]));
+		ByteArrayOutputStream result=setOutPut();
+        TennisReservation.makeReservation(bookDate, Integer.valueOf(inputs[2]), p1,c1);
+        printResult(result);
+    }
+	private static void printResult(ByteArrayOutputStream result) {
+		assertEquals("success",result.toString());	
+	}
 
 	public static void scanner(Scanner s) {
 		int x = s.nextInt();
@@ -27,5 +38,18 @@ public class Main {
         s.close();
 	}
 
+	public static String[] getInput(){
+		System.out.println("Enter your book information(user,time,hour,court): ");
+		Scanner scanner = new Scanner(System.in);
+		String input = scanner.nextLine();
+		System.out.println("your book information:"+input);
+		String[] inputArray=input.split(",");
+		return inputArray;
+	}
+	public static ByteArrayOutputStream setOutPut(){	
+		ByteArrayOutputStream output=new ByteArrayOutputStream();
+		System.setOut(new PrintStream(output));
+		return output;
+	}
     
 }
