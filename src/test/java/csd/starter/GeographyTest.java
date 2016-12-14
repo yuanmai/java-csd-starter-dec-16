a6886afd8e7122c51551b4e6c3aad7f641e295b7
Andy <Andy@Snoopy>
2016-12-14 14:44:43 +0800

    tt

--- src/test/java/csd/starter/GeographyTest.java
+++ src/test/java/csd/starter/GeographyTest.java
@@ -15,14 +15,7 @@ import static org.junit.Assert.assertEquals;
 
 public class GeographyTest {
 
-//
-//    @Test
-//    public void Same_Site() {
-//    	System.out.println("Please input location 1,2,  ");
-//    	Scanner s = new Scanner(System.in).useDelimiter("\\s*,\\s*");
-//    	Location location = inputLocation(s); 
-//        assertEquals(1,Geography.lookup(location).getId());
-//    }
+
     @Test
     public void same_site_should_return_itself() {
 
@@ -49,41 +42,6 @@ public class GeographyTest {
         assertEquals(4,Geography.lookup(location_4).getId());
     }
     
-//    @Test
-//    public void Having2Nearest_Site_Should_Return_Correct_Site() {
-//
-//    	Location location_1 = new Location(20,21);
-//        assertEquals(2,Geography.lookup(location_1).getId());
-//
-//    }
-    
-//    
-//    @Test
-//    public void Same_Site() {
-//    	System.out.println("Please input location x,y,  ");
-//    	Scanner s = new Scanner(System.in).useDelimiter("\\s*,\\s*");
-//    	Location location = inputLocation(s); 
-//        assertEquals(1,Geography.lookup(location).getId());
-//
-//    }
-//    @Test
-//    public void testPod() {
-//    	System.out.println("Please input location x,y,  ");
-//    	Scanner s = new Scanner(System.in).useDelimiter("\\s*,\\s*");
-//    	Location location = inputLocation(s); 
-//        assertEquals(1,Geography.lookup(location).getId());
-//
-//    }
-//    @Test
-//    public void testPod() {
-//    	System.out.println("Please input location x,y,  ");
-//    	Scanner s = new Scanner(System.in).useDelimiter("\\s*,\\s*");
-//    	Location location = inputLocation(s); 
-//        assertEquals(1,Geography.lookup(location).getId());
-//
-//    }
-//    
-//    
     @Test
     public void user_can_input_location(){
     	Tennis tennis = Geography.userSearchTennnis();
@@ -93,7 +51,7 @@ public class GeographyTest {
 	
 
 	@Before
-	public void initial(){
+	public void init(){
 		Tennis.getTennisAssembly().add(new Tennis(1,new Location(1,2)));
 		Tennis.getTennisAssembly().add(new Tennis(2,new Location(10,20)));
 		Tennis.getTennisAssembly().add(new Tennis(3,new Location(30,2)));
