4f915ac48aeb29641f6c093ed7346675901427ba
Zhuo Li <milanello1998@gmail.com>
2016-12-14 13:50:20 +0800

    initial version

---
+++ src/test/java/csd/starter/paymentRecordTester.java
@@ -0,0 +1,27 @@
+package csd.starter;
+
+import java.util.ArrayList;
+
+import org.junit.Test;
+
+public class paymentRecordTester {
+	
+	@Test
+	public void reservPaymentTest(){
+		
+		String payer = "team-d";
+		reservation rr = new reservation();	
+		
+		
+		reservation rr = new reservation(player, court, time_range);
+		paymentRecord pr = payer.pay(rr);
+		player.notification(partner, rr);
+		
+		paymentStatus
+		paymentAmount
+		
+		
+	
+	}
+
+}
---
+++ src/test/java/csd/starter/reservation.java
@@ -0,0 +1,5 @@
+package csd.starter;
+
+public class reservation {
+
+}
