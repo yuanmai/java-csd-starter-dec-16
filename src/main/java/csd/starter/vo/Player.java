ae45e944036c77948d74b6f2514460133c1dd2ea
bapl <bapl1016@gmail.com>
2016-12-15 14:37:17 +0800

    fixed sonar issue

--- src/main/java/csd/starter/vo/Player.java
+++ src/main/java/csd/starter/vo/Player.java
@@ -1,20 +1,23 @@
 package csd.starter.vo;
 
+
+
 /**
  * Created by shushuxu on 2016/12/13.
  */
 public class Player {
-    private String userName;
+	public Player(String userName) {
+		this.userName = userName;
+	}
+	private String userName;
 
-    public String getUserName() {
-        return userName;
-    }
+	public String getUserName() {
+		return userName;
+	}
 
-    public void setUserName(String userName) {
-        this.userName = userName;
-    }
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
 
-    public Player(String userName) {
-        this.userName = userName;
-    }
+
 }
